9755986f8255d6ebc06cc6074f9fadda7887b655
Daniel Reszka <dannn.r@gmail.com>
2014-02-11 19:27:04 +0100

    Implement context ViewingProjects

---
+++ app/contexts/viewing_projects.rb
@@ -0,0 +1,37 @@
+require 'project'
+
+class ViewingProjects
+
+  def initialize(user)
+    @viewer = user
+    @viewer.extend Viewer
+  end
+
+  def view
+    render(@viewer.projects)
+  end
+
+  private
+
+    module Viewer
+      def projects
+        Project.all
+      end
+    end
+
+    def render(project_or_projects)
+      ProjectsPresenter.new(project_or_projects).as_html
+    end
+
+    class ProjectsPresenter
+      def initialize(project_or_projects)
+        @projects = Array(project_or_projects)
+      end
+      def as_html
+        @projects.map do |p|
+          "<li>#{p.name}</li>"
+        end.join
+      end
+    end
+
+end
---
+++ spec/contexts/viewing_projects_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require 'viewing_projects'
+
+describe ViewingProjects do
+
+  context 'given a user' do
+    let(:user) { double('user') }
+    it 'initializes correctly' do
+      context = described_class.new user
+    end
+    describe 'viewing existing projects' do
+      let(:context) { described_class.new(user) }
+      before(:each) do
+        Project.create(name: 'project_1')
+        Project.create(name: 'project_2')
+      end
+      it 'renders the collection of projects' do
+        expect( context.view ).to include 'project_1'
+        expect( context.view ).to include 'project_2'
+      end
+    end
+  end
+
+end

02556f516d07fe10cb12fe45a8998c393763c1b3
Daniel Reszka <dannn.r@gmail.com>
2014-02-12 00:39:12 +0100

    Update spec for HavingAGlobalView
    * Covers rendering the list of people
    * Not testing that users without credentials don't have access, as it's
      covered in specs for sub-contexts (ViewingPeople and ViewingProjects)

--- spec/contexts/having_a_global_view_spec.rb
+++ spec/contexts/having_a_global_view_spec.rb
@@ -2,21 +2,31 @@ require 'spec_helper'
 
 describe HavingAGlobalView do
 
-  context 'given a user' do
-    let(:user) { double('user') }
+  context 'given an user with admin role' do
+    let(:admin) { User.new.tap{ |u| u.add_role :admin } }
     it 'initializes correctly' do
-      context = described_class.new user
+      context = described_class.new admin
     end
     describe 'having a global view' do
+      let(:context) { described_class.new(admin) }
       context('with existing projects') do
-        let(:context) { described_class.new(user) }
         before(:each) do
-        Project.create(name: 'project_A')
-        Project.create(name: 'project_B')
+          Project.create(name: 'project_A')
+          Project.create(name: 'project_B')
         end
         it 'displays the rendered list of projects' do
           expect( context.view ).to include 'project_A'
           expect( context.view ).to include 'project_B'
+        end
+      end
+      context('with existing people') do
+        before(:each) do
+          User.create!(email: "foo1#{rand}@bar.com", password: 'password', name: 'person_A')
+          User.create!(email: "foo2#{rand}@bar.com", password: 'password', name: 'person_B')
+        end
+        it 'displays the rendered list of people' do
+          expect( context.view ).to include 'person_A'
+          expect( context.view ).to include 'person_B'
         end
       end
   end

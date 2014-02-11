782335b7502829da0e4772c91acee42634356501
Daniel Reszka <dannn.r@gmail.com>
2014-02-11 21:06:26 +0100

    Use HavingAGlobalView instead of ViewingProjects on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    render text: ::ViewingProjects.new(current_user).view
+    render text: ::HavingAGlobalView.new(current_user).view
   end
 
 end

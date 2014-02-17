e2ae4b86c4a50ec10c6e3ae6234ec6a33a51dc72
Daniel Reszka <dannn.r@gmail.com>
2014-02-17 17:59:51 +0100

    Make home_controller render directly, without a view file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @global_view = ::HavingAGlobalView.new(current_user, view_context).view
+    render layout: true,
+             text: ::HavingAGlobalView.new(current_user, view_context).view
   end
 
 end

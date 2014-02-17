1aed1585bd9c2a7b4d2f2dc1623443e7a1e1fb64
Daniel Reszka <dannn.r@gmail.com>
2014-02-17 17:03:32 +0100

    Use view_context_spec_helper in specs for ViewingEntries

--- spec/contexts/viewing_entries_spec.rb
+++ spec/contexts/viewing_entries_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
+require 'view_context_spec_helper'
 
 describe ViewingEntries do
+
+  let(:view_context){ view }
 
   describe 'initialization' do
     context 'given a viewer, a view_context, a type of entries and a presenter' do
       let(:viewer){ User.new }
-      let(:view_context){ double('view_context') }
       let(:type){ Class.new }
       let(:presenter){ Class.new }
       it 'initializes correctly' do
@@ -15,14 +17,16 @@ describe ViewingEntries do
   end
 
   describe 'viewing projects' do
+
     let(:subject){ ViewingEntries.new(viewer, view_context, type, presenter)  }
-    let(:view_context){ double('view_context') }
     let(:type){ Project }
     let(:presenter){ ProjectsPresenter }
+
     before(:each) do
       Project.create!(name: 'project_1')
       Project.create!(name: 'project_2')
     end
+
     context 'as a guest viewer' do
       let(:viewer){ User.new }
       it 'does not reveal the projects' do
@@ -30,6 +34,7 @@ describe ViewingEntries do
         expect( subject.reveal ).not_to include 'project_2'
       end
     end
+
     context 'as an authorized viewer' do
       let(:any_authorized_role){ :admin }
       let(:viewer){ User.new.tap{ |u| u.add_role(any_authorized_role) } }
@@ -38,17 +43,21 @@ describe ViewingEntries do
         expect( subject.reveal ).to include 'project_2'
       end
     end
+
   end
 
+
   describe 'viewing people' do
+
     let(:subject){ ViewingEntries.new(viewer, view_context, type, presenter)  }
-    let(:view_context){ double('view_context') }
     let(:type){ User }
     let(:presenter){ PeoplePresenter }
+
     before(:each) do
       User.create!(email: "foo1#{rand}@bar.com", password: 'password', name: 'person_1')
       User.create!(email: "foo1#{rand}@bar.com", password: 'password', name: 'person_2')
     end
+
     context 'as a guest viewer' do
       let(:viewer){ User.new }
       it 'does not reveal the people' do
@@ -56,6 +65,7 @@ describe ViewingEntries do
         expect( subject.reveal ).not_to include 'person_2'
       end
     end
+
     context 'as an authorized viewer' do
       let(:any_authorized_role){ :admin }
       let(:viewer){ User.new.tap{ |u| u.add_role(any_authorized_role) } }
@@ -64,6 +74,7 @@ describe ViewingEntries do
         expect( subject.reveal ).to include 'person_2'
       end
     end
+
   end
 
 

7d2f6c9a05ac9c53a63fff755303ca10b90499f3
Daniel Reszka <dannn.r@gmail.com>
2014-02-17 16:57:29 +0100

    Use view_context_spec_helper in specs for PeoplePresenter

--- spec/presenters/people_presenter_spec.rb
+++ spec/presenters/people_presenter_spec.rb
@@ -1,7 +1,9 @@
+require 'spec_helper'
+require 'view_context_spec_helper'
+
 describe PeoplePresenter do
 
-
-  let(:view_context){ double('view_context') }
+  let(:view_context){ view }
 
   describe 'initialization' do
     context 'given a collection and a view_context' do
@@ -10,28 +12,23 @@ describe PeoplePresenter do
         described_class.new(people, view_context)
       end
     end
-
   end
 
   describe 'rendering to html using a template' do
-    let(:rendered_template){ ERB.new("<%= people.inspect %>").result(binding) }
     context 'given people' do
-      let(:person1){ double('person_1', name: 'person_1') }
-      let(:person2){ double('person_2', name: 'person_2') }
+      let(:person1){ User.new(email: "foo1#{rand}@bar.com", password: 'password', name: 'person_A') }
+      let(:person2){ User.new(email: "foo2#{rand}@bar.com", password: 'password', name: 'person_B') }
       let(:people){ [ person1, person2] }
       let(:subject){ described_class.new(people, view_context) }
-      before(:each) do
-        allow(subject).to receive(:as_html){ rendered_template }
-      end
       it 'passes the people to the template' do
-        expect( subject.as_html ).to include 'person_1'
-        expect( subject.as_html ).to include 'person_2'
+        expect( subject.as_html ).to include 'person_A'
+        expect( subject.as_html ).to include 'person_B'
       end
     end
     describe 'using view helpers' do
-      # it 'supports using rails helpers' do
-      #   pending "I don't know how to test it"
-      # end
+      it 'supports using rails helpers' do
+        pending 'we can now test view helpers using the provided view_context'
+      end
     end
   end
 

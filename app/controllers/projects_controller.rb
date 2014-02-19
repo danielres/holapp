a0c61645f438acd31296754c40a0f6d1b6c2516a
Daniel Reszka <dannn.r@gmail.com>
2014-02-19 20:35:05 +0100

    Render projects_controller#view directly with output from ViewingAnEntry

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,9 @@ class ProjectsController < ApplicationController
   before_filter :authenticate_user!
 
   def show
-    @project = Project.find(params[:id])
+    project = Project.find(params[:id])
+    render layout: true,
+             text: ::ViewingAnEntry.new(current_user, view_context, project).view
   end
 
 end

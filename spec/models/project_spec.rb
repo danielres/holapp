f3792ac122125e1763000fc6c32f4d14858ae62d
Daniel Reszka <dannn.r@gmail.com>
2014-02-24 22:10:01 +0100

    Add description field to Project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -11,4 +11,14 @@ describe Project do
       expect(project.name).to eq 'Foo'
     end
   end
+
+  describe 'attributes' do
+    let(:project){ described_class.new(name: "#{rand}") }
+    it 'has an editable description' do
+      project.description = 'description content'
+      expect(project.description).to eq 'description content'
+    end
+
+  end
+
 end

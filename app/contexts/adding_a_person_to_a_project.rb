61f149dcbb7708b71474a9988c358edc6144794d
Daniel Reszka <dannn.r@gmail.com>
2014-02-24 18:51:29 +0100

    Complete implementation of AddingAPersonToAProject
    * make it receive and pass the view_context
    * make it able to command the controller
    * implement #expose_form

--- app/contexts/adding_a_person_to_a_project.rb
+++ app/contexts/adding_a_person_to_a_project.rb
@@ -1,23 +1,44 @@
+class ActionForbiddenError < StandardError; end
+
 class AddingAPersonToAProject
 
-  def initialize(adder, person, project)
-    @adder = adder
+  def initialize(adder, person, project, view_context)
+    @adder = adder if adder
     @person = person
     @project = project
+    @view_context = view_context
     @adder.extend Adder
-    @project.extend HasMembers
+    @project.extend HasMembers if @project
     @person.extend IsMemberOfProject
   end
 
   def add
-    if @adder.can_manage_memberships?
-      @project.members << @person
-    end
+    @adder.add_person_to_project( @person, @project,
+                       failure: ->(membership){ @controller.try(:failure, membership) },
+                       success: ->(membership){ @controller.try(:success, membership) }, )
+  end
+
+  def expose_form
+    return unless @adder.can_manage_memberships?
+    h.render(partial: 'contexts/adding_a_person_to_a_project/form_from_person', locals: { person: @person })
+  end
+
+  def command(controller)
+    @controller = controller
   end
 
   private
 
+    def h
+      @view_context
+    end
+
     module Adder
+      def add_person_to_project(person, project, callbacks = {})
+        raise ActionForbiddenError unless can_manage_memberships?
+        membership = Membership.new(project: project, user: person)
+        membership.save ? callbacks[:success].call(membership) : callbacks[:failure].call(membership)
+      end
       def can_manage_memberships?
         Ability.new(self).can? :manage, :memberships
       end
@@ -35,8 +56,8 @@ class AddingAPersonToAProject
     module IsMemberOfProject
       def self.extended(object)
         object.class.class_eval do
-          has_many :memberships
-          has_many :project, through: :memberships
+          # has_many :memberships
+          # has_many :project, through: :memberships
         end
       end
     end
--- spec/contexts/adding_a_person_to_a_project_spec.rb
+++ spec/contexts/adding_a_person_to_a_project_spec.rb
@@ -1,32 +1,71 @@
 require 'spec_helper'
+require 'view_context_spec_helper'
 
 describe AddingAPersonToAProject do
 
+  let(:view_context){ view }
+  let(:adding_a_person_to_a_project){ described_class.new(adder, person, project, view_context)  }
+
   describe 'adding a person to a project' do
 
-    let(:adding_a_person_to_a_project){ described_class.new(adder, person, project)  }
     let(:person) do
       User.create!(email: "foo1#{rand}@bar.com", password: 'password', name: 'User name')
     end
     let(:project){ Project.create!(name: 'Project') }
 
-    before(:each) do
-      adding_a_person_to_a_project.add
+    context 'as a guest user' do
+
+      let(:adder){ User.new }
+
+      describe 'exposing the form' do
+        it 'does not render the form' do
+          node = Capybara.string(adding_a_person_to_a_project.expose_form.to_s)
+          expect( node ).not_to have_css 'form.new_membership'
+        end
+      end
+
+      describe 'adding a person to a project' do
+        it 'does not allow the operation to complete' do
+          adding_a_person_to_a_project.add rescue ActionForbiddenError
+        end
+      end
+
     end
 
-    context 'as a guest user' do
-      let(:adder){ User.new }
-      it 'does not add the person to the project' do
-        expect( project.members ).not_to match_array [person]
-      end
-    end
     context 'as an authorized adder' do
       let(:any_authorized_role){ :admin }
       let(:adder){ User.new.tap{ |u| u.add_role(any_authorized_role) } }
+
+      describe 'exposing the form' do
+        it 'renders the form' do
+          node = Capybara.string(adding_a_person_to_a_project.expose_form)
+          expect( node ).to have_css 'form.new_membership'
+        end
+      end
+
       it 'adds the person to the project' do
+        adding_a_person_to_a_project.add
         expect( project.members ).to match_array [person]
       end
+
+      context 'commanding the controller' do
+        let(:controller){ double('controller') }
+        before(:each) do
+          adding_a_person_to_a_project.command(controller)
+        end
+        describe 'on success and on failure' do
+          it 'commands the controller accordingly' do
+            expect(controller).to receive(:success);
+            adding_a_person_to_a_project.add
+            expect(controller).to receive(:failure);
+            adding_a_person_to_a_project.add
+          end
+        end
+      end
+
     end
+
+
   end
 
 end

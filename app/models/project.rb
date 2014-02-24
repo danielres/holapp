f0f9796a76336865515fcb2e2191802d6dc66d0e
Daniel Reszka <dannn.r@gmail.com>
2014-02-24 22:57:40 +0100

    Add validations to Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
   has_many :memberships
   has_many :members, through: :memberships, source: :user
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,6 +12,19 @@ describe Project do
     end
   end
 
+
+  describe 'validations' do
+    it "requires a name" do
+      expect( Project.new(name: '').errors_on(:name) ).to include "can't be blank"
+    end
+    it "requires name to be unique" do
+      p1 = Project.new(name:'myproj').save!
+      p2 = Project.new(name: 'myproj')
+      expect( p2.errors_on(:name) ).to include 'has already been taken'
+    end
+  end
+
+
   describe 'attributes' do
     let(:project){ described_class.new(name: "#{rand}") }
     it 'has an editable description' do

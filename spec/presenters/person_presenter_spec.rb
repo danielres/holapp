c1d5d24e9381214f75323b12ff2f799e4741c1a5
Daniel Reszka <dannn.r@gmail.com>
2014-03-02 20:22:29 +0100

    Complete person_presenter_spec with memberships list and form

--- spec/presenters/person_presenter_spec.rb
+++ spec/presenters/person_presenter_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 require 'view_context_spec_helper'
+require 'fast_authentication_spec_helper'
 
+def admin_user
+  User.create!(email: "admin#{rand}@user.com", password: 'password', name: 'Admin#{rand}').tap do |u|
+    u.confirm!
+    u.add_role :admin
+  end
+end
 def build_random_person options={}
   options = { email: "foo1#{rand}@bar.com", password: 'password', name: 'User name'}.merge options
   User.create!(options)
@@ -26,15 +33,23 @@ describe PersonPresenter do
       expect( person_presenter.to_html ).to include 'Alfred'
     end
     describe "membership" do
-      it "exposes the project membership form" do
-        pending 'todo'
-      end
-      context 'when the person is already a member' do
-        it "displays the memberships" do
-          pending 'todo'
+      let(:viewer){ admin_user }
+      context "when authenticated" do
+        it "exposes the project membership form" do
+          node = Capybara.string( person_presenter.to_html )
+          expect( node ).to have_css("form.new_membership")
+        end
+        context 'when the person is already a project member' do
+          let(:project){ Project.new(name: 'Project name') }
+          before(:each) do
+            person.projects << project
+          end
+          it 'displays the project' do
+            expect( person_presenter.to_html ).to include 'Project name'
+          end
         end
       end
-      end
+    end
   end
 
 end

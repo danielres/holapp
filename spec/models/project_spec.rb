be3edf922b28f7835f0926a8cbcaa944ae6adf99
Daniel Reszka <dannn.r@gmail.com>
2014-03-03 16:24:47 +0100

    Make project_spec simpler using shoudla

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,37 +1,15 @@
 require 'spec_helper'
 
 describe Project do
-  context 'given a name' do
-    it 'initializes correctly' do
-      described_class.new(name: 'Foo')
+
+  describe "model attributes" do
+    it 'has a unique name' do
+      expect(subject).to validate_presence_of(:name)
+      expect(subject).to validate_uniqueness_of(:name)
     end
-
-    it 'has its name correctly set' do
-      project = described_class.new(name: 'Foo')
-      expect(project.name).to eq 'Foo'
+    it 'can have a description' do
+      expect(subject).to respond_to(:description)
     end
-  end
-
-
-  describe 'validations' do
-    it "requires a name" do
-      expect( Project.new(name: '').errors_on(:name) ).to include "can't be blank"
-    end
-    it "requires name to be unique" do
-      p1 = Project.new(name:'myproj').save!
-      p2 = Project.new(name: 'myproj')
-      expect( p2.errors_on(:name) ).to include 'has already been taken'
-    end
-  end
-
-
-  describe 'attributes' do
-    let(:project){ described_class.new(name: "#{rand}") }
-    it 'has an editable description' do
-      project.description = 'description content'
-      expect(project.description).to eq 'description content'
-    end
-
   end
 
 end

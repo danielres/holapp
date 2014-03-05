b73d3e14731609775b10eb1927e9bebe45bbc9d2
Daniel Reszka <dannn.r@gmail.com>
2014-03-05 15:25:44 +0100

    Add support for short syntax 'expect_it'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,4 +50,17 @@ RSpec.configure do |config|
   end
 end
 
+
+# support for short syntax expect_it
+RSpec.configure do |c|
+  c.alias_example_to :expect_it
+end
+RSpec::Core::MemoizedHelpers.module_eval do
+  alias to should
+  alias to_not should_not
+end
+
+
 I18n.enforce_available_locales = false
+
+

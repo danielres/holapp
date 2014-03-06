bf1ca329ae7048ba9991d4dc94604eaf674455bd
Daniel Reszka <dannn.r@gmail.com>
2014-03-06 14:20:00 +0100

    Make uniqueness of project name case insensitive

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
 
   validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: { case_sensitive: false }
 
   has_many :memberships
   has_many :members, through: :memberships, source: :user
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -14,7 +14,7 @@ describe Project do
 
   describe 'validations' do
     expect_it { to validate_presence_of(:name) }
-    expect_it { to validate_uniqueness_of(:name) }
+    expect_it { to validate_uniqueness_of(:name).case_insensitive }
   end
 
 end

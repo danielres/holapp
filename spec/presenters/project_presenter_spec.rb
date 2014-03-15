47b055c430aa49d287e3f03652ce2df1c5e05240
Daniel Reszka <dannn.r@gmail.com>
2014-03-15 22:44:29 +0100

    Add coverage for project description field in project_presenter_spec

--- spec/presenters/project_presenter_spec.rb
+++ spec/presenters/project_presenter_spec.rb
@@ -9,10 +9,13 @@ describe ProjectPresenter do
   describe 'rendering to html' do
     subject{ described_class.new(viewer, project, view_context) }
     let(:viewer){ build(:no_roles_user) }
-    let(:project){ build(:project, name: 'My project') }
+    let(:project){ build(:project, name: 'My project', description: 'Project description') }
     let(:view_context){ view }
-    it 'presents the project' do
+    it 'presents the project name' do
       expect( fragment subject.to_html ).to have_content('My project')
+    end
+    it 'presents the project description' do
+      expect( fragment subject.to_html ).to have_content('Project description')
     end
   end
 

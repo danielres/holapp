8494e65c80ae5dfcc22d921cbf56850847e31b73
Daniel Reszka <dannn.r@gmail.com>
2014-03-16 20:21:55 +0100

    Add missing spec for model Tagging

--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -1,3 +1,10 @@
 describe Tagging do
 
+  describe 'attributes' do
+    expect_it { to have_attribute('tag_id') }
+    expect_it { to have_attribute('taggable_id') }
+    expect_it { to have_attribute('taggable_type') }
+    expect_it { to have_attribute('context') }
+  end
+
 end

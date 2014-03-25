4b06f19e89fe90ffa5a3494b5e830141bd234757
Daniel Reszka <dannn.r@gmail.com>
2014-03-25 13:58:11 +0100

    Improve ordering of taggings
    * Orders taggings on taggable pages (project and person page) by
      quantifier value, with taggings having null quantifiers last

--- app/contexts/viewing_a_taggable_taggings.rb
+++ app/contexts/viewing_a_taggable_taggings.rb
@@ -29,7 +29,10 @@ class ViewingATaggableTaggings
         Ability.new(self).can? :read, taggable
       end
       def available_taggings(taggable, tag_field)
-        Tagging.where(taggable_id: taggable.id, taggable_type: taggable.class.name, context: tag_field)
+        taggings = Tagging.where(taggable_id: taggable.id, taggable_type: taggable.class.name, context: tag_field)
+        nonull   = taggings.where("quantifier is not null").order(quantifier: :desc)
+        yesnull  = taggings.where("quantifier is null")
+        nonull + yesnull
       end
     end
 

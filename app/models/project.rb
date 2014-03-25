cf9b58fb0eda9a95a9ddbd33b26b03afedc25145
Daniel Reszka <dannn.r@gmail.com>
2014-03-25 20:52:52 +0100

    Fix for table column "type" on projects already reserved by ActiveRecord

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,11 @@
 class Project < ActiveRecord::Base
 
+  self.inheritance_column = "disabled"
+
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
 
   has_many :memberships
   has_many :members, through: :memberships, source: :user
+
 end

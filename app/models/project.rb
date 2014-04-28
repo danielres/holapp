06c6f14d989afe6acb17e56ff341ccc2e324443e
Daniel Reszka <dannn.r@gmail.com>
2014-04-28 09:34:45 +0200

    Implement DestroyingAProject
    * destroys the project and related associations (memberships and taggings)

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,9 @@ class Project < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
 
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
+  has_many :taggings, as: :taggable, dependent: :destroy
+
   has_many :members, through: :memberships, source: :user
 
   def type

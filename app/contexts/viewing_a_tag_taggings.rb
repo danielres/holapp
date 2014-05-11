3818fa0b6b1f9f354b93eb1f2afa449d22e88701
Daniel Reszka <dannn.r@gmail.com>
2014-05-11 12:47:59 +0200

    Extract ViewingATagTaggings html-building code to TagTaggingsPresenter

--- app/contexts/viewing_a_tag_taggings.rb
+++ app/contexts/viewing_a_tag_taggings.rb
@@ -8,27 +8,10 @@ class ViewingATagTaggings
 
   def expose_taggings_by_taggable_types(view_context)
     raise ActionForbiddenError unless @viewer.can_view_taggables? @tag
-    output = []
-    taggings.group_by(&:taggable_type).map do |type, taggings|
-      title = type.pluralize
-      title = 'People' if title == 'Users'
-      output << "<section data-purpose='#{ title.downcase }-taggings'>"
-      output << "<h1>#{ title }</h1>"
-      taggings.group_by(&:context).map do |tag_field, taggings|
-        output << "<div class='panel'>"
-        output << TagFieldWithTaggingsPresenter.new(tag_field: tag_field, taggings: taggings, view_context: view_context).to_html(viewed_from: :tag)
-        output << "</div>"
-      end
-      output << "</section>"
-    end
-    "<section>#{ output.join }</section>".html_safe
+    TagTaggingsPresenter.new(taggings: @tag.taggings, view_context: view_context).to_html #todo: pass the tag, not the taggings
   end
 
   private
-
-    def taggings
-      @tag.taggings
-    end
 
     module Viewer
       def can_view_taggables? tag
---
+++ app/presenters/tag_taggings_presenter.rb
@@ -0,0 +1,25 @@
+class TagTaggingsPresenter < Erector::Widget
+
+  needs :taggings, :view_context
+
+  include Support::PresenterHelpers
+
+  def content
+    @taggings.group_by(&:taggable_type).map do |type, taggings|
+      title = domain_language(type).pluralize
+      section the( "#{ title.downcase }-taggings" ) do
+        h1 title
+        taggings.group_by(&:context).map do |tag_field, taggings|
+          panel do
+            text TagFieldWithTaggingsPresenter
+                   .new( tag_field: tag_field,
+                          taggings: taggings,
+                      view_context: @view_context )
+                   .to_html(viewed_from: :tag)
+          end
+        end
+      end
+    end
+  end
+
+end

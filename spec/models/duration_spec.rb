e69a5c2c069b45a239a491e89895043680cae3cb
Daniel Reszka <dannn.r@gmail.com>
2014-06-10 15:18:42 +0200

    Implement durations for memberships

---
+++ spec/models/duration_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Duration do
+
+  describe 'attributes' do
+    expect_it { to have_attribute('durable_id') }
+    expect_it { to have_attribute('durable_type') }
+    expect_it { to have_attribute('starts_at') }
+    expect_it { to have_attribute('ends_at') }
+  end
+
+end

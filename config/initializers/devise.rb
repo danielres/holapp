64f539741f60145abe99b317e1c820d2d0872b61
Daniel Reszka <dannn.r@gmail.com>
2014-07-03 16:50:57 +0200

    Set session timeout to 1000 minutes in development
    * Makes life easier for developers

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -170,7 +170,7 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  config.timeout_in = 10.minutes
+  config.timeout_in = Rails.env.development? ? 1000.minutes : 10.minutes
 
   # If true, expires auth token on session timeout.
   # config.expire_auth_token_on_timeout = false

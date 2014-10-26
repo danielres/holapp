6f4d3f8c5be0f16b0689d96be8a8a67be401ce0f
Daniel Reszka <dannn.r@gmail.com>
2014-10-26 16:15:54 +0100

    Add feature spec for 'Deleting a news item'

---
+++ spec/features/news/deleting_a_news_item_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require 'fast_authentication_spec_helper'
+require 'purpose_selector_spec_helper'
+require 'factories_spec_helper'
+
+require 'capybara/poltergeist'
+
+describe 'Deleting a news item', :slow, :news, driver: :poltergeist do
+  let(:super_user){ create(:super_user) }
+  let!(:news_item){ create(:news_item, summary: 'The summary', body: 'The body') }
+
+  context 'as superuser' do
+
+    describe 'deleting the news item' do
+      before(:each) do
+        login_as(super_user, scope: :user)
+        visit news_path
+        within the('news_items-list') do
+          find( the 'delete-action').click
+          sleep 0.1
+        end
+      end
+      it %q[destroys the news item] do
+        expect( News::Item.count ).to eq 0
+      end
+    end
+
+  end
+
+end

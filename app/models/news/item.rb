fef32ce7ef53d36543a05240b95ac1bbdca6f1bb
Daniel Reszka <dannn.r@gmail.com>
2014-10-27 19:22:17 +0100

    Add author_id and taggings support to news item (only at model level)

--- app/models/news/item.rb
+++ app/models/news/item.rb
@@ -3,4 +3,11 @@ class News::Item < ActiveRecord::Base
   validates_presence_of  :summary
   validates_presence_of  :language
   validates_inclusion_of :language, in: LANGUAGES
+
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :taggings, as: :taggable, dependent: :destroy
+
+  def name       ; summary end
+  def to_s       ; name  end
+
 end
--- spec/models/news/item_spec.rb
+++ spec/models/news/item_spec.rb
@@ -4,7 +4,12 @@ describe News::Item, :news do
   describe 'attributes' do
     expect_it { to validate_presence_of('summary') }
     expect_it { to have_attribute('body') }
+    expect_it { to have_attribute('author_id') }
     expect_it { to validate_presence_of('language') }
     expect_it { to ensure_inclusion_of(:language).in_array(%w(fr en)) }
+  end
+  describe 'associations' do
+    expect_it { to belong_to(:author) }
+    expect_it { to have_many(:taggings).dependent(:destroy) }
   end
 end

07baae1c6c4affa620934231bf66bd45b034dffd
Daniel Reszka <dannn.r@gmail.com>
2014-11-20 11:37:01 +0100

    Improve MarkdownRenderer
    * make it accept nil input
    * make its output html_safe

--- lib/markdown_renderer.rb
+++ lib/markdown_renderer.rb
@@ -3,7 +3,7 @@ SHOWDOWN_JS = File.read( Rails.root.join("vendor/assets/javascripts/showdown.js"
 class MarkdownRenderer
 
   def call(string)
-    execution_context.call(converter_js, string)
+    execution_context.call(converter_js, string.to_s).html_safe
   end
 
   private

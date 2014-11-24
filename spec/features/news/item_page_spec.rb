d0189b9be61df369317bb06e5128f7ff1801e2f4
Daniel Reszka <dannn.r@gmail.com>
2014-11-24 15:05:32 +0100

    Add missing tag :news to feature spec

--- spec/features/news/item_page_spec.rb
+++ spec/features/news/item_page_spec.rb
@@ -3,7 +3,7 @@ require 'fast_authentication_spec_helper'
 require 'factories_spec_helper'
 require 'purpose_selector_spec_helper'
 
-describe 'Item page', :slow do
+describe 'Item page', :news, :slow do
   let(:news_item){ create(:news_item) }
 
 

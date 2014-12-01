f061efed8388f532542cdc6aaa16eadabedbd637
Daniel Reszka <dannn.r@gmail.com>
2014-12-01 16:04:16 +0100

    Implement news preferences in a generic user preferences system

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121130955) do
+ActiveRecord::Schema.define(version: 20141127151253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,14 @@ ActiveRecord::Schema.define(version: 20141121130955) do
     t.datetime "updated_at"
     t.string   "language",   default: "en"
     t.integer  "author_id"
+  end
+
+  create_table "news_user_configs", force: true do |t|
+    t.integer  "user_id"
+    t.datetime "digest_sent_at"
+    t.boolean  "receive_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "projects", force: true do |t|

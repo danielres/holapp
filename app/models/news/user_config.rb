f39b28873333dfa78102e28d379c5c4e95efc224
Daniel Reszka <dannn.r@gmail.com>
2014-12-03 14:30:11 +0100

    Remove validating presence of user_id in News::UserConfig

--- app/models/news/user_config.rb
+++ app/models/news/user_config.rb
@@ -1,4 +1,3 @@
 class News::UserConfig < ActiveRecord::Base
   belongs_to :user, dependent: :destroy
-  validates :user_id, presence: true
 end
--- spec/models/news/user_config_spec.rb
+++ spec/models/news/user_config_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe News::UserConfig, :news do
   describe 'attributes' do
-    expect_it { to validate_presence_of('user_id') }
     expect_it { to have_attribute('digest_sent_at') }
     expect_it { to have_attribute('receive_digest') }
   end

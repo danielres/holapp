4ca2cf254c6f1eb2d8b5ee6af75c9fc9be1b23d2
Daniel Reszka <dannn.r@gmail.com>
2014-12-16 16:21:57 +0100

    Improve page "Activities", add spec coverage

--- app/presenters/activities_presenter.rb
+++ app/presenters/activities_presenter.rb
@@ -6,7 +6,7 @@ class ActivitiesPresenter < Erector::Widget
 
   def content
       row do
-        col(8){ panel{ activities_table(@collection, 'Activities') } }
+        col(12){ panel{ activities_table(@collection, 'Activities') } }
       end
   end
 
@@ -14,8 +14,15 @@ class ActivitiesPresenter < Erector::Widget
   private
 
     def activities_table collection, caption_text
-      table('data-purpose' => 'activities-list') do
+      table('data-purpose' => 'activities-list', class: 'activities-list' ) do
         caption caption_text
+        tr do
+          th.time    'Time'
+          th.user    'User'
+          th.action  'Action'
+          th.subject 'Subject'
+          th.details 'Details'
+        end
         collection.each do |a|
           tr do
             td.time @view_context.time_ago_in_words(a.created_at) + " ago"
@@ -32,11 +39,11 @@ class ActivitiesPresenter < Erector::Widget
       case a.action
       when 'deleted'
         td "deleted"
-        td object_type
+        td object_type.sub('::',': ')
         td object_name
       when 'updated'
         td 'updated'
-        td "#{a.object.class.name.downcase} #{ a.details.keys.join(', ')}"
+        td "#{a.object.class.name.downcase.sub('::',': ')} #{ a.details.keys.join(', ')}"
         td.object do
           text link_to(a.object, a.object) if a.object
         end
@@ -50,7 +57,7 @@ class ActivitiesPresenter < Erector::Widget
       when 'added_taggings'
         td "tagged on #{ a.details['tag_field'] }"
         td.object do
-          text link_to(a.object, a.object) if a.object
+          text link_to(a.object.to_s.truncate(60), a.object) if a.object
         end
         td "#{ a.details['tag_list'].split(',').map{|t| @view_context.link_to_if(Tag.find_by_name(t), t, Tag.find_by_name(t) )}.join(', ') }".html_safe
       when 'merged_tags'

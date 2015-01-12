c32caea7ea39c0f7dfc4600f3293222b5c65b4de
Daniel Reszka <dannn.r@gmail.com>
2015-01-12 14:58:07 +0100

    Improve news digests email: add "uninteresting" news as "other news"

--- app/mailers/news/mailer.rb
+++ app/mailers/news/mailer.rb
@@ -1,11 +1,11 @@
 class News::Mailer < ActionMailer::Base
   default from: ENV['MAILER_FROM_ADDRESS'] || "from@example.com"
 
-  def digest_email(user, news_items)
+  def digest_email(user, news_items_interesting, news_items_other)
     subject     = "Your Marketplace news digest"
     mail(to: user.email, subject: subject) do |format|
       format.html do
-        render locals: { news_items: news_items, user: user}
+        render locals: { news_items_interesting: news_items_interesting, news_items_other: news_items_other, user: user}
       end
     end
   end
--- app/use_cases/news/receiving_a_digest_email.rb
+++ app/use_cases/news/receiving_a_digest_email.rb
@@ -10,8 +10,10 @@ module News
     def initialize(recipient_user, dry_run: false)
       @recipient  = recipient_user
       @config     = News::UserConfig.where(user: @recipient).first_or_create
-      @news_items = News::Fetcher.new(@recipient, 'interesting', @config.digest_sent_at).call
-      @email      = News::Mailer.digest_email( @recipient, @news_items)
+      @all_news_items           = News::Fetcher.new(@recipient, 'all'        , @config.digest_sent_at).call
+      @interesting_news_items   = News::Fetcher.new(@recipient, 'interesting', @config.digest_sent_at).call
+      @uninteresting_news_items = @all_news_items - @interesting_news_items
+      @email      = News::Mailer.digest_email(@recipient, @interesting_news_items, @uninteresting_news_items)
       @dry_run    = dry_run
     end
 
@@ -19,7 +21,9 @@ module News
       begin
         super
       rescue ActionForbiddenError => e
-        msg = "\nCANCELED #{self.class} for user '#{@recipient}': #{e.message}"
+        msg = "\n--------------------------------------------------------------------------------"
+        msg << "\n"
+        msg << "CANCELED #{self.class} for user '#{@recipient}': #{e.message}"
         puts   msg
         return msg
       end
@@ -28,15 +32,24 @@ module News
     private
 
       def execution
-        msg = "\n"
-        msg << "Sending digest with #{ @news_items.count } news to #{ @recipient.name.ljust(20, ' ') } \n"
-        msg << @news_items.map{|n| n.id.to_s + ": " + n.summary.truncate(80) }.join("\n")  if @news_items.any?
+        msg = "\n--------------------------------------------------------------------------------"
+        msg << "\n"
+        msg << "Sending digest with #{ @interesting_news_items.count } (interesting) + #{ @uninteresting_news_items.count } (other) news to #{ @recipient.name.ljust(20, ' ') } \n"
+        msg << "\n"
+        msg << "Interesting:"
+        msg << "\n"
+        msg << @interesting_news_items.map{|n| n.id.to_s + ": " + n.summary.truncate(80) }.join("\n")  if @interesting_news_items.any?
+        msg << "\n"
+        msg << "Other:"
+        msg << "\n"
+        msg << @uninteresting_news_items.map{|n| n.id.to_s + ": " + n.summary.truncate(80) }.join("\n")  if @uninteresting_news_items.any?
+        msg << "\n"
         if @config.receive_digest == false
           msg << 'Canceled: user refuses digests'
           puts msg
           return msg
         end
-        if @news_items.empty?
+        if @all_news_items.empty?
           msg << 'Canceled: no fresh news'
           puts msg
           return msg
@@ -54,7 +67,7 @@ module News
       def journal_event
         return {} if @dry_run
         return {} if @config.receive_digest == false
-        return {} if @news_items.empty?
+        return {} if @all_news_items.empty?
         {}
       end
 

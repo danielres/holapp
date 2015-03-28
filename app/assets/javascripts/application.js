8d49bb55d72cbc2c3711e087c58b60ac1ef30981
Daniel Reszka <dannn.r@gmail.com>
2015-03-28 20:10:29 +0100

    bundle update jquery-ui-rails (4.1.1 to 5.0.3)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.ui.datepicker
+//= require jquery-ui/datepicker
 //= require jquery_ujs
 //= require angular
 //= require sugar
